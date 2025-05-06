2b1cbc5a9ff105f15ee5546ac10c1ae8bad1d665
Mustaphamexy <mustaphamexy111@gmail.com>
2025-05-06 22:51:04 +0100

    Initial commit - project setup with React, Tailwind, Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline bg-amber-600">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
