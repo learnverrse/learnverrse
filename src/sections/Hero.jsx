b71b151123689b410239dbdd06ae9bbc7b6ece14
Ochife Ogechukwu <ochifeoge@gmail.com>
2025-05-09 20:06:58 +0100

    chore: update dependencies and add Prettier configuration
    - Added eslint-config-prettier, eslint-plugin-prettier, eslint-plugin-react, and prettier to package.json for improved code quality and formatting.
    - Updated eslint-plugin-react-refresh to version 0.4.20.
    - Updated Hero.jsx for consistent styling and formatting, including changes to class names and structure.
    - Created .prettierignore to exclude specific directories from formatting.
    - Added .prettierrc configuration file for Prettier with specified rules and plugins.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React from 'react';
 import {
   heroImg,
   logo,
   navLinks,
   radialGradient,
   search,
-} from "../components/details";
-import { Link } from "react-router";
-import Button from "../components/UI/Button";
-import { FaBars } from "react-icons/fa";
+} from '../components/details';
+import { Link } from 'react-router';
+import Button from '../components/UI/Button';
+import { FaBars } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <header className="min-h-screen bg-white container">
+    <header className="container min-h-screen bg-white">
       {/* navbar */}
-      <nav className="flex items-center justify-between px-6 py-4 max-w-7xl mx-auto">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
         <div className="flex items-center gap-x-2">
           {/* logo */}
           <img src={logo} alt="Learnverrse's Logo" />
 
           {/* navlinks */}
-          <div className=" md:ml-4   w-full bg-white  flex">
+          <div className="flex w-full bg-white md:ml-4">
             {navLinks.map(({ name, path }, index) => {
               <Link className="nav-links" to={`${path}`} key={index}>
                 {name}
@@ -30,27 +30,27 @@ const Hero = () => {
         </div>
 
         {/* search input */}
-        <div className="w-full hidden lg:block max-w-md relative mx-4">
+        <div className="relative mx-4 hidden w-full max-w-md lg:block">
           <input
             type="text"
             placeholder="Discover Courses To Learn"
-            className="w-full px-4 py-2 pl-10 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-700"
+            className="w-full rounded-full border border-gray-300 px-4 py-2 pl-10 focus:ring-2 focus:ring-purple-700 focus:outline-none"
           />
 
           <img
             src={search}
             alt="Search icon"
-            className="absolute left-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+            className="absolute top-1/2 left-3 -translate-y-1/2 transform cursor-pointer"
           />
         </div>
 
         {/* buttons */}
-        <div className="md:flex gap-4">
-          <div className="hidden items-center space-x-4  md:flex">
-            <Button active={false} label={"sign in"} fun={() => {}} />
-            <Button active={true} label={"join us now"} fun={() => {}} />
+        <div className="gap-4 md:flex">
+          <div className="hidden items-center space-x-4 md:flex">
+            <Button active={false} label={'sign in'} fun={() => {}} />
+            <Button active={true} label={'join us now'} fun={() => {}} />
           </div>
-          <button className="lg:hidden flex items-center justify-center w-10 h-10 bg-purple-200 rounded-full hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-700">
+          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-purple-200 hover:bg-gray-300 focus:ring-2 focus:ring-purple-700 focus:outline-none lg:hidden">
             <FaBars />
           </button>
         </div>
@@ -58,24 +58,24 @@ const Hero = () => {
 
       {/* hero section */}
 
-      <div className=" container mx-auto px-6">
-        <div className="flex flex-col text-center lg:text-start lg:flex-row items-center justify-between w-full">
-          <div className="md:w-1/2 mb-6 md:mb-0">
-            <h1 className="text-5xl md:text-7xl font-bold mb-4 capitalize">
-              Where curious minds meet{" "}
+      <div className="container mx-auto px-6">
+        <div className="flex w-full flex-col items-center justify-between text-center lg:flex-row lg:text-start">
+          <div className="mb-6 md:mb-0 md:w-1/2">
+            <h1 className="mb-4 text-5xl font-bold capitalize md:text-7xl">
+              Where curious minds meet{' '}
               <span className="text-purple-700">expert</span> guides.
             </h1>
-            <p className="text-lg text-black mb-8 max-w-lg mt-8">
-              {" "}
+
+            <p className="mt-8 mb-8 flex max-w-lg text-lg text-black">
               Fuel your curiosity with guidance from experts who’ve walked the
               path—learn deeply, grow confidently, and turn potential into
               progress.
             </p>
 
-            <div className="flex justify-center lg:justify-start space-x-4">
-              <Button label={"see plans"} active={true} fun={() => {}} />
+            <div className="z-20 flex justify-center space-x-4 lg:justify-start">
+              <Button label={'see plans'} active={true} fun={() => {}} />
               <Button
-                label={" Access your course"}
+                label={' Access your course'}
                 active={false}
                 fun={() => {}}
               />
@@ -83,7 +83,7 @@ const Hero = () => {
           </div>
 
           {/* img */}
-          <div className="md:w-1/2 flex justify-center">
+          <div className="pointer-events-none flex justify-center md:w-1/2">
             <img
               src={heroImg}
               alt="register now!!!"
