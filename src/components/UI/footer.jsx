5ca16bed0c66fa2e709d3af47350732303fcdf73
Mustaphamexy <mustaphamexy111@gmail.com>
2025-07-05 09:14:48 +0100

    Landing page width fix

--- src/components/UI/footer.jsx
+++ src/components/UI/footer.jsx
@@ -1,129 +1,164 @@
-import { FiChevronDown, FiChevronsDown } from "react-icons/fi";
+import { FiChevronDown, FiChevronsDown } from 'react-icons/fi';
 
-import { 
-    logo2, 
-    footerLine, 
-    linkedIn, 
-    faceBook, 
-    instagram, 
-    xTwitter, 
-} from "../details";
-import { useState } from "react";
+import {
+  logo2,
+  footerLine,
+  linkedIn,
+  faceBook,
+  instagram,
+  xTwitter,
+} from '../details';
+import { useState } from 'react';
 
 function Footer() {
-    const [openDropdown, setOpenDropdown] = useState(null);
+  const [openDropdown, setOpenDropdown] = useState(null);
 
-    const toggleDropdown = (section) => {
-        setOpenDropdown(openDropdown === section ? null : section);
-    };
+  const toggleDropdown = (section) => {
+    setOpenDropdown(openDropdown === section ? null : section);
+  };
 
-    return (
-        <footer className="container bg-[#121212] py-10 lg:px-10 lg:py-20 w-full">
-            <div className="m-auto flex w-[90%] md:w-[95%] lg:w-[90%] flex-col items-start justify-between md:flex-row">
-                <div className="flex flex-col space-y-6 text-white mb-10 order-1 md:hidden">
-                    <img src={logo2} alt="logo" />
-                </div>
+  return (
+    <footer className="w-full bg-[#121212] py-10 lg:py-20">
+      <div className="container mx-auto w-[90%] md:w-[95%] lg:w-[90%]">
+        <div className="flex flex-col items-start justify-between md:flex-row">
+          <div className="order-1 mb-10 flex flex-col space-y-6 text-white md:hidden">
+            <img src={logo2} alt="logo" />
+          </div>
 
-                <div className="flex flex-col space-y-6 text-white order-5 md:order-none mb-6 md:mb-0 md:w-[20%] lg:w-auto md:text-[13px] lg:text-base">
-                    <img src={logo2} alt="logo" className="hidden md:block md:w-[80%] lg:w-full"/>
-                    <a href="#">About us</a>
-                    <a href="#">Careers</a>
-                    <a href="#">Blog</a>
-                </div>
+          <div className="order-5 mb-6 flex flex-col space-y-6 text-white md:order-none md:mb-0 md:w-[20%] md:text-[13px] lg:w-auto lg:text-base">
+            <img
+              src={logo2}
+              alt="logo"
+              className="hidden md:block md:w-[80%] lg:w-full"
+            />
+            <a href="#">About us</a>
+            <a href="#">Careers</a>
+            <a href="#">Blog</a>
+          </div>
 
-                <div className="flex flex-col space-y-6 text-white w-full md:w-[20%] lg:w-auto order-2 md:order-none">
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-xl md:text-[15px] lg:text-xl font-bold">For Learner</h2>
-                        <button className="md:hidden" onClick={() => toggleDropdown("learner")}>
-                            <FiChevronDown className={`${openDropdown === "learner" ? "rotate-180" : ""} transition-transform`} />
-                        </button>
-                    </div>
-                    <div className={`${openDropdown === "learner" ? "flex" : "hidden"} md:flex flex-col space-y-6 mb-4 md:mb-0 md:text-[13px] lg:text-base`}>
-                        <a href="#">Browse Courses</a>
-                        <a href="#">How It Works</a>
-                        <a href="#">Pricing</a>
-                        <a href="#">Help Center</a>
-                    </div>
-                </div>
-
-                <div className="flex flex-col space-y-6 text-white w-full md:w-[20%] lg:w-auto order-3 md:order-none">
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-xl md:text-[15px] lg:text-xl font-bold">For Educator</h2>
-                        <button className="md:hidden" onClick={() => toggleDropdown("educator")}>
-                            <FiChevronDown className={`${openDropdown === "educator" ? "rotate-180" : ""} transition-transform`} />
-                        </button>
-                    </div>
-                    <div className={`${openDropdown === "educator" ? "flex" : "hidden"} md:flex flex-col space-y-6 mb-4 md:mb-0 md:text-[13px] lg:text-base`}>
-                        <a href="#">Become an educator</a>
-                        <a href="#">Educator Dashboard</a>
-                        <a href="#">Community</a>
-                    </div>
-                </div>
-
-                <div className="flex flex-col space-y-6 text-white w-full md:w-[20%] lg:w-auto order-4 md:order-none">
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-xl md:text-[15px] lg:text-xl font-bold">Legal & Support</h2>
-                        <button className="md:hidden" onClick={() => toggleDropdown("legal")}>
-                            <FiChevronDown className={`${openDropdown === "legal" ? "rotate-180" : ""} transition-transform`} />
-                        </button>
-                    </div>
-                    <div className={`${openDropdown === "legal" ? "flex" : "hidden"} md:flex flex-col space-y-6 mb-4 md:mb-0 md:text-[13px] lg:text-base`}>
-                        <a href="#">Forms of Services</a>
-                        <a href="#">Privacy Policy</a>
-                        <a href="#">Cookies settings</a>
-                    </div>
-                </div>
-                <div className="flex flex-col space-y-6 text-white order-6 md:order-none">
-                    <h2 className="text-xl md:text-[15px] lg:text-xl font-bold">Contact Us</h2>
-                    <input
-                    type="email"
-                    name="email"
-                    id=""
-                    placeholder="Enter your email address"
-                    className="placeholder:text-ourGray outline-primary-800 bg-white p-2 text-black md:max-w-sm lg:w-auto"
-                    ></input>
-                </div>
+          <div className="order-2 flex w-full flex-col space-y-6 text-white md:order-none md:w-[20%] lg:w-auto">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl font-bold md:text-[15px] lg:text-xl">
+                For Learner
+              </h2>
+              <button
+                className="md:hidden"
+                onClick={() => toggleDropdown('learner')}
+              >
+                <FiChevronDown
+                  className={`${openDropdown === 'learner' ? 'rotate-180' : ''} transition-transform`}
+                />
+              </button>
             </div>
-
-            <div className="m-auto mt-16 w-full md:w-[95%] lg:w-[90%]">
-                <img src={footerLine} alt="" />
+            <div
+              className={`${openDropdown === 'learner' ? 'flex' : 'hidden'} mb-4 flex-col space-y-6 md:mb-0 md:flex md:text-[13px] lg:text-base`}
+            >
+              <a href="#">Browse Courses</a>
+              <a href="#">How It Works</a>
+              <a href="#">Pricing</a>
+              <a href="#">Help Center</a>
             </div>
+          </div>
 
-            <div className="m-auto mt-10 flex flex-col space-y-5 md:space-y-0 md:flex-row w-[90%] md:w-[95%] lg:w-[90%] items-center justify-between text-white">
-                <div className="flex items-center space-x-3">
-                    <a href="#">
-                        <img src={linkedIn} alt="" className=""/>
-                    </a>
-                    <a href="#">
-                        <img src={faceBook} alt="" />
-                    </a>
-                    <a href="#">
-                        <img src={instagram} alt="" />
-                    </a>
-                    <a href="#">
-                        <img src={xTwitter} alt="" />
-                    </a>  
-                </div>
-                <div>
-                    <p className="text-sm md:text-base">
-                        2025{" "}
-                        <span className="font-bold">
-                            Learn<span className="text-primary-600">verrse</span>
-                        </span>
-                        . All rights Reserved.
-                    </p>
-                </div>
-                <div className="hidden md:flex items-center space-x-3">
-                    <div className="border-r-1 px-3">
-                    <a href="#">Privacy Policy</a>
-                    </div>
-                    <div>
-                    <a href="#">Terms of Service</a>
-                    </div>
-                </div>
+          <div className="order-3 flex w-full flex-col space-y-6 text-white md:order-none md:w-[20%] lg:w-auto">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl font-bold md:text-[15px] lg:text-xl">
+                For Educator
+              </h2>
+              <button
+                className="md:hidden"
+                onClick={() => toggleDropdown('educator')}
+              >
+                <FiChevronDown
+                  className={`${openDropdown === 'educator' ? 'rotate-180' : ''} transition-transform`}
+                />
+              </button>
             </div>
-        </footer>
-    )
+            <div
+              className={`${openDropdown === 'educator' ? 'flex' : 'hidden'} mb-4 flex-col space-y-6 md:mb-0 md:flex md:text-[13px] lg:text-base`}
+            >
+              <a href="#">Become an educator</a>
+              <a href="#">Educator Dashboard</a>
+              <a href="#">Community</a>
+            </div>
+          </div>
+
+          <div className="order-4 flex w-full flex-col space-y-6 text-white md:order-none md:w-[20%] lg:w-auto">
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl font-bold md:text-[15px] lg:text-xl">
+                Legal & Support
+              </h2>
+              <button
+                className="md:hidden"
+                onClick={() => toggleDropdown('legal')}
+              >
+                <FiChevronDown
+                  className={`${openDropdown === 'legal' ? 'rotate-180' : ''} transition-transform`}
+                />
+              </button>
+            </div>
+            <div
+              className={`${openDropdown === 'legal' ? 'flex' : 'hidden'} mb-4 flex-col space-y-6 md:mb-0 md:flex md:text-[13px] lg:text-base`}
+            >
+              <a href="#">Forms of Services</a>
+              <a href="#">Privacy Policy</a>
+              <a href="#">Cookies settings</a>
+            </div>
+          </div>
+          <div className="order-6 flex flex-col space-y-6 text-white md:order-none">
+            <h2 className="text-xl font-bold md:text-[15px] lg:text-xl">
+              Contact Us
+            </h2>
+            <input
+              type="email"
+              name="email"
+              id=""
+              placeholder="Enter your email address"
+              className="placeholder:text-ourGray outline-primary-800 bg-white p-2 text-black md:max-w-sm lg:w-auto"
+            ></input>
+          </div>
+        </div>
+
+        <div className="mt-16 w-full">
+          <img src={footerLine} alt="" />
+        </div>
+
+        <div className="mt-10 flex flex-col items-center justify-between space-y-5 text-white md:flex-row md:space-y-0">
+          <div className="flex items-center space-x-3">
+            <a href="#">
+              <img src={linkedIn} alt="" className="" />
+            </a>
+            <a href="#">
+              <img src={faceBook} alt="" />
+            </a>
+            <a href="#">
+              <img src={instagram} alt="" />
+            </a>
+            <a href="#">
+              <img src={xTwitter} alt="" />
+            </a>
+          </div>
+          <div>
+            <p className="text-sm md:text-base">
+              2025{' '}
+              <span className="font-bold">
+                Learn<span className="text-primary-600">verrse</span>
+              </span>
+              . All rights Reserved.
+            </p>
+          </div>
+          <div className="hidden items-center space-x-3 md:flex">
+            <div className="border-r-1 px-3">
+              <a href="#">Privacy Policy</a>
+            </div>
+            <div>
+              <a href="#">Terms of Service</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/sections/FaqSection.jsx
+++ src/sections/FaqSection.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import {chevronRight, faqHuman} from '../components/details';
+import React, { useState } from 'react';
+import { chevronRight, faqHuman } from '../components/details';
 
 // const FaqSection = () => {
 //   return (
@@ -8,35 +8,35 @@ import {chevronRight, faqHuman} from '../components/details';
 //               Frequently Asked Questions
 //             </h2>
 //             <p className="text-base lg:text-2xl">What questions do you need answered?</p>
-    
+
 //             <div className="m-auto mt-8 flex w-[90%] lg:w-[70%] flex-col-reverse items-center gap-y-6 md:gap-y-0 justify-between lg:px-4 md:flex-row md:p-6">
 //               <div className="flex flex-col w-[90%] lg:w-[50%] space-y-5">
 //                 <div className="bg-primary-50 flex cursor-pointer items-center justify-between rounded-4xl px-6 py-5 lg:px-10 lg:py-3 hover:bg-purple-200">
 //                   <p className='text-[14px]'>What is Learnverrse</p>
 //                   <img src={chevronRight} alt="" className='hidden md:block' />
 //                 </div>
-    
+
 //                 <div className="bg-primary-50 flex cursor-pointer items-center justify-between rounded-4xl px-6 py-5 lg:px-10 lg:py-3 hover:bg-purple-200">
 //                   <p className='text-[14px]'>How do I sign up</p>
 //                   <img src={chevronRight} alt="" className='hidden md:block' />
 //                 </div>
-    
+
 //                 <div className="bg-primary-50 flex cursor-pointer items-center justify-between rounded-4xl px-6 py-5 lg:px-10 lg:py-3 hover:bg-purple-200">
 //                   <p className='text-[14px]'>What payment methods are accepted</p>
 //                   <img src={chevronRight} alt="" className='hidden md:block' />
 //                 </div>
-    
+
 //                 <div className="bg-primary-50 flex cursor-pointer items-center justify-between rounded-4xl px-6 py-5 lg:px-10 lg:py-3 hover:bg-purple-200">
 //                   <p className='text-[14px]'>Can I cancel my subscriptions at anytime</p>
 //                   <img src={chevronRight} alt="" className='hidden md:block' />
 //                 </div>
-    
+
 //                 <div className="bg-primary-50 flex cursor-pointer items-center justify-between rounded-4xl px-6 py-5 lg:px-10 lg:py-3 hover:bg-purple-200">
 //                   <p className='text-[14px]'>How do I get paid as an educator</p>
 //                   <img src={chevronRight} alt="" className='hidden md:block' />
 //                 </div>
 //               </div>
-    
+
 //               <div className="w-[60%] lg:w-[30%]">
 //                 <img src={faqHuman} alt="" className=''/>
 //               </div>
@@ -53,69 +53,75 @@ function FaqSection() {
   };
 
   return (
-    <div className="container flex flex-col items-center justify-center py-10 lg:py-16">
-      <h2 className="mb-4 text-2xl lg:text-[40px] font-bold text-[#121212]">
-        Frequently Asked Questions
-      </h2>
-      <p className="text-base lg:text-2xl">What questions do you need answered?</p>
+    <div className="flex w-full flex-col items-center justify-center py-12  lg:py-16">
+      <div className="container flex flex-col items-center justify-center">
+        <h2 className="mb-4 text-2xl font-bold text-[#121212] lg:text-[40px]">
+          Frequently Asked Questions
+        </h2>
+        <p className="text-base lg:text-2xl">
+          What questions do you need answered?
+        </p>
 
-      <div className="m-auto mt-8 flex w-[90%] md:w-[95%] lg:w-[70%] flex-col-reverse items-center gap-y-6 md:gap-y-0 justify-between lg:px-4 md:flex-row md:p-6">
-        <div className="flex flex-col w-[90%] lg:w-[50%] space-y-5">
-          {[
-            {
-              id: 1,
-              question: "What is Learnverrse?",
-              answer: "Learnverrse is an online learning platform where learners access expert-led courses and educators share knowledge, build communities, and monetize their expertise."
-            },
-            {
-              id: 2,
-              question: "How do I sign up?",
-              answer: "Click the 'Sign Up' button on our homepage, provide your details, and you're ready to explore courses or create your educator profile."
-            },
-            {
-              id: 3,
-              question: "What payment methods are accepted?",
-              answer: "We accept major payment methods including Debit/Credit Cards, PayPal, Bank Transfers, and Digital Wallets where supported."
-            },
-            {
-              id: 4,
-              question: "Can I cancel my subscriptions at any time?",
-              answer: "Yes, subscriptions can be canceled anytime from your dashboard. Access remains active until the end of your current billing cycle."
-            },
-            {
-              id: 5,
-              question: "How do I get paid as an educator?",
-              answer: "Educators earn through course sales, live sessions, and community memberships. Payouts are processed monthly via bank transfer or PayPal."
-            }
-          ].map(({id, question, answer}) => (
-            <div key={id}>
-              <div
-                onClick={() => toggleQuestion(id)}
-                className="bg-primary-50 flex cursor-pointer items-center justify-between rounded-4xl px-6 py-5 lg:px-10 lg:py-3 hover:bg-purple-200"
-              >
-                <p className='text-[14px]'>{question}</p>
-                <img 
-                  src={chevronRight} 
-                  alt='toggle' 
-                  className={`hidden lg:block transform transition-transform ${openQuestion === id ? "rotate-90" : "rotate-0"}`} 
-                />
-              </div>
-              {openQuestion === id && (
-                <div className="p-3 text-sm mt-2">
-                  {answer}
+        <div className="m-auto mt-8 flex w-[90%] flex-col-reverse items-center justify-between gap-y-6 md:w-[95%] md:flex-row md:gap-y-0 md:p-6 lg:w-[70%] lg:px-4">
+          <div className="flex w-[90%] flex-col space-y-5 lg:w-[50%]">
+            {[
+              {
+                id: 1,
+                question: 'What is Learnverrse?',
+                answer:
+                  'Learnverrse is an online learning platform where learners access expert-led courses and educators share knowledge, build communities, and monetize their expertise.',
+              },
+              {
+                id: 2,
+                question: 'How do I sign up?',
+                answer:
+                  "Click the 'Sign Up' button on our homepage, provide your details, and you're ready to explore courses or create your educator profile.",
+              },
+              {
+                id: 3,
+                question: 'What payment methods are accepted?',
+                answer:
+                  'We accept major payment methods including Debit/Credit Cards, PayPal, Bank Transfers, and Digital Wallets where supported.',
+              },
+              {
+                id: 4,
+                question: 'Can I cancel my subscriptions at any time?',
+                answer:
+                  'Yes, subscriptions can be canceled anytime from your dashboard. Access remains active until the end of your current billing cycle.',
+              },
+              {
+                id: 5,
+                question: 'How do I get paid as an educator?',
+                answer:
+                  'Educators earn through course sales, live sessions, and community memberships. Payouts are processed monthly via bank transfer or PayPal.',
+              },
+            ].map(({ id, question, answer }) => (
+              <div key={id}>
+                <div
+                  onClick={() => toggleQuestion(id)}
+                  className="bg-primary-50 flex cursor-pointer items-center justify-between rounded-4xl px-6 py-5 hover:bg-purple-200 lg:px-10 lg:py-3"
+                >
+                  <p className="text-[14px]">{question}</p>
+                  <img
+                    src={chevronRight}
+                    alt="toggle"
+                    className={`hidden transform transition-transform lg:block ${openQuestion === id ? 'rotate-90' : 'rotate-0'}`}
+                  />
                 </div>
-              )}
-            </div>
-          ))}
-        </div>
+                {openQuestion === id && (
+                  <div className="mt-2 p-3 text-sm">{answer}</div>
+                )}
+              </div>
+            ))}
+          </div>
 
-        <div className="w-[60%] lg:w-[30%] lg:self-start">
-          <img src={faqHuman} alt="FAQ Illustration" className='' />
+          <div className="w-[60%] lg:w-[30%] lg:self-start">
+            <img src={faqHuman} alt="FAQ Illustration" className="" />
+          </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default FaqSection
+export default FaqSection;
